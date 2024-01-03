0ea9947290d2f9dd26bc50a656e4766ff7c4fb13
Elkhan Mammadli <elkhantma@code.edu.az>
2024-01-03 20:41:49 +0400

    Created Array Methods

--- ConsoleAppTask/Program.cs
+++ ConsoleAppTask/Program.cs
@@ -1,10 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using Random_Examples.Examples;
 using TaskIntegralType.Entities;
 
 Console.WriteLine("Hello, World!");
 
 #region Task 1: Integer Operations
-
 TaskIntegerOperations myTask = new TaskIntegerOperations();
 //myTask.Addition();
 //myTask.Decrement();
@@ -19,4 +19,16 @@ TaskIntegerOperations myTask = new TaskIntegerOperations();
 //myTask.Modulo();
 //myTask.Multiplication();
 //myTask.Subtraction();
+#endregion
+#region ArrayExamples
+ArrayExample myExample = new ArrayExample();
+//int[] myInt = new int[] { 1, 5, 6, 7, 5, 2 };
+//myExample.OddAndEven(myInt);
+
+//Console.Write("Enter the word: ");
+//string myWord = Console.ReadLine();
+//if (String.IsNullOrEmpty(myWord)) throw new ArgumentNullException(nameof(myWord));
+//Console.Write("Enter the number for repeating: ");
+//int myNum = Convert.ToInt32(Console.ReadLine());
+//myExample.StringArrayExample(myWord,myNum);
 #endregion
---
+++ Random Examples/Examples/ArrayExample.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Random_Examples.Examples;
+
+public class ArrayExample
+{
+    public void OddAndEven(int[] arr)
+    {
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            if (arr[i] % 2 == 0)
+            {
+                Console.WriteLine($"This is even number: {arr[i]}");
+            }
+            else if (arr[i] % 2 == 1)
+            {
+                Console.WriteLine($"This is odd number: {arr[i]}");
+            }
+            else
+            {
+                throw new ArgumentNullException();
+            }
+        }
+    }
+
+    public string StringArrayExample(string word, int countOfWords)
+    {
+        string result = String.Empty;
+        for (int i = 0;i < countOfWords;i++)
+        {
+            result = result + word;
+        }
+        Console.WriteLine(result.ToLower());
+        return result;
+    }
+}
